8fa69a90700bb3a80a49381365c8fcbd7b0696aa
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-07-15 20:39:51 -0400

    created JournalView class, and populated default functions that will be ovverridden

---
+++ app/src/main/java/zeidler/colin/rocketjournal/JournalView.java
@@ -0,0 +1,41 @@
+package zeidler.colin.rocketjournal;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Colin on 2014-07-15.
+ *
+ * Activity to display the detailed contents of a single rocket journal entry
+ * specific Journal is based on the one selected from the JournalListView
+ */
+public class JournalView extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class JournalViewFrag extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return super.onCreateView(inflater, container, savedInstanceState);
+        }
+    }
+}
