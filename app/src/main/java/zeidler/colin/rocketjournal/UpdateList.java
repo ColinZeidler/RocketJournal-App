142edd52c93929c0e5500525d5c70062bde13802
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-08-09 16:45:33 -0400

    adding documentation

--- app/src/main/java/zeidler/colin/rocketjournal/UpdateList.java
+++ app/src/main/java/zeidler/colin/rocketjournal/UpdateList.java
@@ -2,6 +2,8 @@ package zeidler.colin.rocketjournal;
 
 /**
  * Created by Colin on 2014-08-07.
+ *
+ * Interface for Fragments that have a List that can be modified by an external source
  */
 public interface UpdateList {
     public void updateList();
